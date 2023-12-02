63bd24c8d6fb7a90e30c0369ab13eca2c19f98e3
DiegoAlvaradoUCA <00127522@uca.edu.sv>
2023-12-01 22:01:06 -0600

    fix

--- API-WEB/server/routes/router.js
+++ API-WEB/server/routes/router.js
@@ -3,6 +3,7 @@ const route = express.Router()
 
 
 const controller = require('../controller/controller');
+const services = require('../services/render');
 const packageController = require('../controller/package.controller')
 
 /**
@@ -25,6 +26,12 @@ route.get('/update-user', services.update_user)
 
 
 // API
+
+route.post('/api/users', controller.create);
+route.get('/api/users', controller.find);
+route.put('/api/users/:id', controller.update);
+route.delete('/api/users/:id', controller.delete);
+
 route.post('/api/packages', packageController.create);
 route.get('/api/packages', packageController.find);
 route.put('/api/packages/:id', packageController.update);
